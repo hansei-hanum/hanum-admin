7b9e27e5af3c4cdfb6989340b600e5339d3e0276
wonjjani <108343303+wonjjani@users.noreply.github.com>
2024-09-27 16:42:32 +0900

    haha whatis this

--- .history/hanum_ad/settings_20240924184749.py
+++ .history/hanum_ad/settings_20240924184749.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6fckmlw_xy5%hwe+3ssk6gz8dt7clo$$a#5$i5wg7o1bdg3@h8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
